094b42a00c7b7937ef9b91834d04acbb0a9ed637
JTK01 <99972138+JTK01@users.noreply.github.com>
2022-10-16 19:37:33 +0900

    Add files via upload

---
+++ element_replacement.py
@@ -0,0 +1,14 @@
+N,K = map(int,input().split())
+list1 = list(map(int,input().split()))
+list2 = list(map(int,input().split()))
+
+list1.sort()
+list2.sort(reverse=True)
+
+for i in range(K):
+    if list1[i] < list2[i]:
+        list1[i], list2[i] = list2[i], list1[i]
+    else:
+        break
+
+print(sum(list1))
